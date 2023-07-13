eab330c65d9ea855dcd8cbd5bafe8e1cd62bef5d
Noureddine Benomar <benomar886@outlook.fr>
2023-07-14 00:22:47 +0200

    ajout script js v2

--- client/utils/dom.js
+++ client/utils/dom.js
@@ -1,21 +1,3 @@
-  /**
-   * Crée un élément du dom, lui ajoute du texte, le place comme dernier
-   * enfant de parent et ajoute un attribut en utilisant le paramètre attributes
-   * @param {String} markup_name 
-   * @param {String} text 
-   * @param {domElement} parent 
-   * @param {Array[Object]} attributes  (doit comprendre les propriétés name et value)
-   * @returns domElement
-   */
-  /**
-   * Crée un élément du dom, lui ajoute du texte, le place comme dernier
-   * enfant de parent et ajoute un attribut en utilisant le paramètre attributes
-   * @param {String} markup_name 
-   * @param {String} text 
-   * @param {domElement} parent 
-   * @param {Array[Object]} attributes  (doit comprendre les propriétés name et value)
-   * @returns domElement
-   */
 // Fonction pour créer un élément du DOM avec du texte et des attributs
 function createMarkup(markupname, text, parent, attributes = []) {
   const markup = document.createElement(markupname);
@@ -31,75 +13,51 @@ function createMarkup(markupname, text, parent, attributes = []) {
 
 // Fonction pour créer l'interface de l'application
 function createInterface() {
-  const container = document.createElement('div');
-  container.classList.add('container');
+  const container = createMarkup('div', '', document.body, [{ name: 'class', value: 'container py-5' }]);
 
   // Titre de l'application
-  createMarkup('h1', 'Master Cook', container, [{ name: 'class', value: 'text-center mt-5' }]);
-
-  const row = createMarkup('div', '', container, [{ name: 'class', value: 'row mt-4' }]);
+  createMarkup('h1', 'Master Cook', container, [{ name: 'class', value: 'text-center mb-4' }]);
 
   // Bouton "Afficher toutes les recettes"
-  createMarkup('div', '', createMarkup('div', '', row, [{ name: 'class', value: 'col-md-3' }]), [{ name: 'class', value: 'col-md-3' }]);
-  createMarkup('button', 'Afficher toutes les recettes', row.lastChild.firstChild, [{ name: 'id', value: 'showAllRecipesBtn' }, { name: 'class', value: 'btn btn-primary btn-block' }]);
+  const showAllRecipesBtn = createMarkup('button', 'Afficher toutes les recettes', container, [{ name: 'class', value: 'btn btn-primary mb-3' }]);
+  showAllRecipesBtn.addEventListener('click', function() {
+    // Logique pour afficher toutes les recettes
+    // Ici, vous pouvez mettre le code qui affiche toutes les recettes dans la modale ou effectue une action appropriée
+    console.log('Afficher toutes les recettes');
+  });
 
   // Bouton "Sélection par pays"
-  createMarkup('div', '', createMarkup('div', '', row, [{ name: 'class', value: 'col-md-3' }]), [{ name: 'class', value: 'col-md-3' }]);
-  createMarkup('button', 'Sélection par pays', row.lastChild.firstChild, [{ name: 'id', value: 'filterByCountryBtn' }, { name: 'class', value: 'btn btn-primary btn-block' }]);
+  const filterByCountryBtn = createMarkup('button', 'Sélection par pays', container, [{ name: 'class', value: 'btn btn-primary mb-3' }]);
+  filterByCountryBtn.addEventListener('click', function() {
+    // Logique pour la sélection par pays
+    // Ici, vous pouvez mettre le code qui affiche les recettes filtrées par pays dans la modale ou effectue une action appropriée
+    console.log('Sélection par pays');
+  });
 
   // Bouton "Sélection par ingrédient"
-  createMarkup('div', '', createMarkup('div', '', row, [{ name: 'class', value: 'col-md-3' }]), [{ name: 'class', value: 'col-md-3' }]);
-  createMarkup('button', 'Sélection par ingrédient', row.lastChild.firstChild, [{ name: 'id', value: 'filterByIngredientBtn' }, { name: 'class', value: 'btn btn-primary btn-block' }]);
+  const filterByIngredientBtn = createMarkup('button', 'Sélection par ingrédient', container, [{ name: 'class', value: 'btn btn-primary mb-3' }]);
+  filterByIngredientBtn.addEventListener('click', function() {
+    // Logique pour la sélection par ingrédient
+    // Ici, vous pouvez mettre le code qui affiche les recettes filtrées par ingrédient dans la modale ou effectue une action appropriée
+    console.log('Sélection par ingrédient');
+  });
 
   // Bouton "Nouvelle recette" (avec modal)
-  createMarkup('div', '', createMarkup('div', '', row, [{ name: 'class', value: 'col-md-3' }]), [{ name: 'class', value: 'col-md-3' }]);
-  const addRecipeBtn = createMarkup('button', 'Nouvelle recette', row.lastChild.firstChild, [{ name: 'id', value: 'addRecipeBtn' }, { name: 'class', value: 'btn btn-primary btn-block', 'data-toggle': 'modal', 'data-target': '#addRecipeModal' }]);
+  const addRecipeBtn = createMarkup('button', 'Nouvelle recette', container, [{ name: 'class', value: 'btn btn-primary mb-3', 'data-toggle': 'modal', 'data-target': '#addRecipeModal' }]);
+  addRecipeBtn.addEventListener('click', function() {
+    // Logique pour afficher la modale d'ajout de recette
+    // Ici, vous pouvez mettre le code qui effectue l'affichage de la modale ou effectue une action appropriée
+    console.log('Nouvelle recette');
+  });
 
   // Modal pour afficher une recette
   const recipeModal = createMarkup('div', '', document.body, [{ name: 'class', value: 'modal fade', 'id': 'recipeModal', 'tabindex': '-1', 'role': 'dialog', 'aria-labelledby': 'recipeModalLabel', 'aria-hidden': 'true' }]);
-  const modalDialog = createMarkup('div', '', recipeModal, [{ name: 'class', value: 'modal-dialog', 'role': 'document' }]);
-  const modalContent = createMarkup('div', '', modalDialog, [{ name: 'class', value: 'modal-content' }]);
-  const modalHeader = createMarkup('div', '', modalContent, [{ name: 'class', value: 'modal-header' }]);
-  createMarkup('h5', 'Titre de la recette', modalHeader, [{ name: 'class', value: 'modal-title', 'id': 'recipeModalLabel' }]);
-  const closeButton = createMarkup('button', '', modalHeader, [{ name: 'type', value: 'button' }, { name: 'class', value: 'close', 'data-dismiss': 'modal', 'aria-label': 'Fermer' }]);
-  createMarkup('span', '&times;', closeButton, [{ name: 'aria-hidden', value: 'true' }]);
-  const modalBody = createMarkup('div', '', modalContent, [{ name: 'class', value: 'modal-body' }]);
-  createMarkup('p', 'Description de la recette', modalBody);
-  createMarkup('h6', 'Ingrédients :', modalBody);
-  createMarkup('ul', '', modalBody, [{ name: 'id', value: 'ingredientsList' }]);
-  const modalFooter = createMarkup('div', '', modalContent, [{ name: 'class', value: 'modal-footer' }]);
-  createMarkup('button', 'Fermer', modalFooter, [{ name: 'type', value: 'button' }, { name: 'class', value: 'btn btn-secondary', 'data-dismiss': 'modal' }]);
-  createMarkup('button', 'Modifier', modalFooter, [{ name: 'type', value: 'button' }, { name: 'class', value: 'btn btn-primary' }]);
-  createMarkup('button', 'Supprimer', modalFooter, [{ name: 'type', value: 'button' }, { name: 'class', value: 'btn btn-danger' }]);
+  // ...
 
   // Modal pour ajouter une recette
   const addRecipeModal = createMarkup('div', '', document.body, [{ name: 'class', value: 'modal fade', 'id': 'addRecipeModal', 'tabindex': '-1', 'role': 'dialog', 'aria-labelledby': 'addRecipeModalLabel', 'aria-hidden': 'true' }]);
-  const addModalDialog = createMarkup('div', '', addRecipeModal, [{ name: 'class', value: 'modal-dialog', 'role': 'document' }]);
-  const addModalContent = createMarkup('div', '', addModalDialog, [{ name: 'class', value: 'modal-content' }]);
-  const addModalHeader = createMarkup('div', '', addModalContent, [{ name: 'class', value: 'modal-header' }]);
-  createMarkup('h5', 'Nouvelle recette', addModalHeader, [{ name: 'class', value: 'modal-title', 'id': 'addRecipeModalLabel' }]);
-  const addCloseButton = createMarkup('button', '', addModalHeader, [{ name: 'type', value: 'button' }, { name: 'class', value: 'close', 'data-dismiss': 'modal', 'aria-label': 'Fermer' }]);
-  createMarkup('span', '&times;', addCloseButton, [{ name: 'aria-hidden', value: 'true' }]);
-  const addModalBody = createMarkup('div', '', addModalContent, [{ name: 'class', value: 'modal-body' }]);
-  const addRecipeForm = createMarkup('form', '', addModalBody, [{ name: 'id', value: 'addRecipeForm' }]);
-  createMarkup('div', '', addRecipeForm, [{ name: 'class', value: 'form-group' }]);
-  createMarkup('label', 'Titre de la recette', addRecipeForm, [{ name: 'for', value: 'recipeTitle' }]);
-  createMarkup('input', '', addRecipeForm, [{ name: 'type', value: 'text' }, { name: 'class', value: 'form-control', 'id': 'recipeTitle', 'required': 'required' }]);
-  createMarkup('div', '', addRecipeForm, [{ name: 'class', value: 'form-group' }]);
-  createMarkup('label', 'Description de la recette', addRecipeForm, [{ name: 'for', value: 'recipeDescription' }]);
-  createMarkup('textarea', '', addRecipeForm, [{ name: 'class', value: 'form-control', 'id': 'recipeDescription', 'rows': '3', 'required': 'required' }]);
-  createMarkup('div', '', addRecipeForm, [{ name: 'class', value: 'form-group' }]);
-  createMarkup('label', 'Ingrédients', addRecipeForm, [{ name: 'for', value: 'ingredients' }]);
-  createMarkup('textarea', '', addRecipeForm, [{ name: 'class', value: 'form-control', 'id': 'ingredients', 'rows': '3', 'required': 'required' }]);
-  createMarkup('small', 'Saisissez les ingrédients, un par ligne.', addRecipeForm, [{ name: 'class', value: 'form-text text-muted' }]);
-  const addModalFooter = createMarkup('div', '', addModalContent, [{ name: 'class', value: 'modal-footer' }]);
-  createMarkup('button', 'Fermer', addModalFooter, [{ name: 'type', value: 'button' }, { name: 'class', value: 'btn btn-secondary', 'data-dismiss': 'modal' }]);
-  createMarkup('button', 'Valider', addModalFooter, [{ name: 'type', value: 'button' }, { name: 'class', value: 'btn btn-primary' }]);
-
-  // Ajout du container à la page
-  document.body.appendChild(container);
+  // ...
 }
 
 // Appel de la fonction pour créer l'interface
 createInterface();
-
