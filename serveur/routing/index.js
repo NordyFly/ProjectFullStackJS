a83cde099a144e6becf1889f3e9c856b2c8ee05c
Jacques Granarolo <jac.granarolo.info@gmail.com>
2023-07-15 13:48:50 +0200

    ajout de la doc du serveur : 'documentation-serveur.txt'

--- serveur/routing/index.js
+++ serveur/routing/index.js
@@ -6,18 +6,8 @@ const {
   deleteRecipesCtrl,
 } = require('../controllers/app.ctrl');
 
-
-
 // Récupérer toutes les recettes / Attention : les parametre apres '?' sont pris en compte dans cette route
 router.get('/recipes', recipesCtrl);
-
-// ci dessous : ne sert pas, ne prend pas en compte le parametre apres le ? de l'url de la requete
-// Filtrer les recettes par gastronomie et ingrédients
-//router.get('/recipes/gastronomy', filterGastronomy);
-
-// ci dessous : ne sert pas, ne prend pas en compte le parametre apres le ? de l'url de la requete
-//router.get('/recipes/ingredients', filterIngredients);
-
 // Créer une nouvelle recette
 router.post('/recipes', createRecipeCtrl);
 // Modifier une recette existante
